2f00b4e0925fc339868ee427313fd842017fe5b1
aussig <austin@goudges.com>
2019-04-25 11:28:09 +0100

    Remove references to now defunct 'outline' ionicons

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,17 +1,17 @@
 /**
  * AnchorBuddy.  A mobile app to help with anchor calculations.
  * Copyright (C) 2018  Austin Goudge and Stephen Gorst
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -48,19 +48,22 @@ export default TabNavigator(
           case 'Anchor':
             iconName =
               Platform.OS === 'ios'
-                ? `ios-boat${focused ? '' : '-outline'}`
+                ? `ios-boat`
+                /*? `ios-boat${focused ? '' : '-outline'}`*/
                 : 'md-boat';
             break;
           case 'Help':
             iconName =
               Platform.OS === 'ios'
-                ? `ios-help-circle${focused ? '' : '-outline'}`
+                ? `ios-help-circle`
+                /*? `ios-help-circle${focused ? '' : '-outline'}`*/
                 : 'md-help-circle';
             break;
           case 'About':
             iconName =
               Platform.OS === 'ios'
-                ? `ios-information-circle${focused ? '' : '-outline'}`
+                ? `ios-information-circle`
+                /*? `ios-information-circle${focused ? '' : '-outline'}`*/
                 : 'md-information-circle';
             break;
         }
